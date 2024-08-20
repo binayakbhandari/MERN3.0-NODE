e07f4c300bc83570fa54c6862db6c280f1a616af
binayakbhandari <binayakbhandari60@gmail.com>
2024-08-20 09:04:38 +0545

    push

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const connectDatabase = require('./database')
+const Blog = require('./model/blogModel')
 
 const app = express()
 app.use(express.json())
@@ -13,8 +14,21 @@ app.get("/", (req, res)=>{
     })
 })
 
-app.post("/blog", (req, res)=>{
-    console.log(req.body)
+app.post("/blog",async (req, res)=>{
+    // console.log(req.body)
+    // const title = req.body.title
+    // const subtitle = req.body.subtitle
+    // const description = req.body.description
+    // const image = req.body.image
+    // We can do same thing in this way which is called object destructuring
+    const {title, subtitle, description, image} = req.body
+    await Blog.create({
+        title : title,
+        subtitle : subtitle,
+        description : description,
+        image : image
+    })
+    console.log(title)
     res.status(200).json({
         message : "Blog API hit sucessfully"
     })
