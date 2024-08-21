4a61e8de58d70a2829227443d092cf768ffbc561
binayakbhandari <binayakbhandari60@gmail.com>
2024-08-21 18:46:17 +0545

    learn about request file, get api

--- app.js
+++ app.js
@@ -16,31 +16,40 @@ app.get("/", (req, res)=>{
 })
 
 app.post("/blog",upload.single('image'), async (req, res)=>{
-    // console.log(req.body)
-    // const title = req.body.title
-    // const subtitle = req.body.subtitle
-    // const description = req.body.description
-    // const image = req.body.image
     // We can do same thing in this way which is called object destructuring
-    const {title, subtitle, description, image} = req.body
-    // if(!title || !subtitle || !description || !image){
-    //     return res.status(400).json({
-    //         message : "Please provide title, subtitle, description or image"
-    //     })
-    // }
+    const {title, subtitle, description} = req.body
+    // console.log(req.body)
+    // console.log(req.file)
+    const filename = req.file.filename
+
+    if(!title || !subtitle || !description){
+        return res.status(400).json({
+            message : "Please provide title, subtitle, description or image"
+        })
+    }
 
     await Blog.create({
         title: title,
         subtitle: subtitle,
         description: description,
-        image: image
+        image: filename
     })
 
-    console.log(title)
     res.status(200).json({
         message: "Blog API hit sucessfully"
     })
 })
+
+app.get("/blog", async (req,res)=>{
+    const blogs = await Blog.find() // find() returns data in an aray
+    res.status(200).json({
+        message : "Blog data fetched successfully",
+        data : blogs
+    })
+})
+
+
+app.use(express.static('./storage'))
 
 app.listen(process.env.PORT, () => {
     console.log("NodeJS project has started")
